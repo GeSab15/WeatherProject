64f6afd307769e9eb845d1369dfe1ef51dcf4613
GeSab15 <72660952+GeSab15@users.noreply.github.com>
2021-02-24 20:59:18 +0000

    list instead of main

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ celciusButton.addEventListener("click", celcius);
 
 function dayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
-  let forecast = response.data.main[0];
+  let forecast = response.data.list[0];
   forecastElement.innerHTML = `
   <div class="col2">
   <h2>0000H</h2>
